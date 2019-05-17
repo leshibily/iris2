01e2bfa97bbc69b466b4d4aadf590b6caf3eb1fa
Serhii Pronoza <spronoza@mozilla.com>
2019-05-17 12:44:08 +0300

    cnn_heavy_content ported. Excluded because of change_preferences didn't work as expected

--- tests/firefox/plugin_compatibility/cnn_heavy_content.py
+++ tests/firefox/plugin_compatibility/cnn_heavy_content.py
@@ -13,6 +13,7 @@ class Test(FirefoxTest):
         locale=['en-US'],
         test_case_id='125534',
         test_suite_id='2074',
+        enabled=False
     )
     def run(self, firefox):
         new_private_browsing_tab_pattern = PrivateWindow.private_window_pattern
@@ -23,43 +24,47 @@ class Test(FirefoxTest):
         cnn_weather_page_tab_pattern = Pattern('cnn_logo_tab.png')
 
         home_width, home_height = NavBar.HOME_BUTTON.get_size()
-        tabs_region = Region(0, 0, Screen.SCREEN_WIDTH/2, home_height * 4)
+        tabs_region = Rectangle(0, 0, Screen.SCREEN_WIDTH/2, home_height * 4)
 
-        change_preference('media.autoplay.default', '0')
+        change_preference('media.autoplay.default', '0')  # It seems that is not work as expected
 
         new_private_window()
 
-        private_window_opened = exists(new_private_browsing_tab_pattern, Settings.SITE_LOAD_TIMEOUT)
+        # private_window_opened = exists(new_private_browsing_tab_pattern, Settings.SITE_LOAD_TIMEOUT)
+        private_window_opened = exists(new_private_browsing_tab_pattern, Settings.DEFAULT_FIREFOX_TIMEOUT*3)
         assert private_window_opened is True, 'A new private window is successfully opened'
 
         navigate('http://www.cnn.com/2016/10/10/us/weather-matthew/index.html')
 
-        cnn_weather_page_loaded = exists(cnn_weather_page_tab_pattern, Settings.HEAVY_SITE_LOAD_TIMEOUT, tabs_region)
+        # cnn_weather_page_loaded = exists(cnn_weather_page_tab_pattern, Settings.HEAVY_SITE_LOAD_TIMEOUT, tabs_region)
+        cnn_weather_page_loaded = exists(cnn_weather_page_tab_pattern, Settings.DEFAULT_FIREFOX_TIMEOUT*9, tabs_region)
         assert cnn_weather_page_loaded is True, 'The specified website is successfully loaded.'
 
-        video_playing = exists(speaker_icon_pattern, Settings.SITE_LOAD_TIMEOUT)
+        # video_playing = exists(speaker_icon_pattern, Settings.SITE_LOAD_TIMEOUT)
+        video_playing = exists(speaker_icon_pattern, Settings.DEFAULT_FIREFOX_TIMEOUT*3)
         assert video_playing is True, 'The video is playing and the speaker icon is displayed'
 
-        first_video_centred = scroll_until_pattern_found(north_text_mark_pattern, type, (Key.DOWN,), 20)
+        first_video_centred = scroll_until_pattern_found(north_text_mark_pattern, 100, 20)
         assert first_video_centred is True, 'First video is centred among the page'
 
-        video_window = find(north_text_mark_pattern).above(100)
-
-        click(video_window)
+        click(north_text_mark_pattern.target_offset(0, -100))
 
         try:
-            speaker_icon_vanished = wait_vanish(speaker_icon_pattern, Settings.SITE_LOAD_TIMEOUT, tabs_region)
-            play_icon_appeared = exists(play_icon_pattern, Settings.SITE_LOAD_TIMEOUT)
+            # speaker_icon_vanished = wait_vanish(speaker_icon_pattern, Settings.SITE_LOAD_TIMEOUT, tabs_region)
+            # play_icon_appeared = exists(play_icon_pattern, Settings.SITE_LOAD_TIMEOUT
+            speaker_icon_vanished = wait_vanish(speaker_icon_pattern, Settings.DEFAULT_FIREFOX_TIMEOUT*3, tabs_region)
+            play_icon_appeared = exists(play_icon_pattern, Settings.DEFAULT_FIREFOX_TIMEOUT*3)
             assert (speaker_icon_vanished and play_icon_appeared) is True, 'Video is stopped'
         except FindError:
             raise FindError('Video is not stopped')
 
-        another_video_exists = scroll_until_pattern_found(related_video_pattern, page_down, (None,), 20)
+        another_video_exists = scroll_until_pattern_found(related_video_pattern, 500, 20)
         assert another_video_exists is True, 'Another video is displayed'
 
         click(related_video_pattern)
 
-        related_video_playing = exists(speaker_icon_pattern, Settings.SITE_LOAD_TIMEOUT, tabs_region)
+        # related_video_playing = exists(speaker_icon_pattern, Settings.SITE_LOAD_TIMEOUT, tabs_region)
+        related_video_playing = exists(speaker_icon_pattern, Settings.DEFAULT_FIREFOX_TIMEOUT*3, tabs_region)
         assert related_video_playing is True, 'The video is playing and there is no browser crashes'
 
         close_window()
